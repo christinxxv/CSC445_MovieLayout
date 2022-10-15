aa951ef43de5435281a6cc550cdb82479bc4cce8
christinxxv <cwilson39@missuouriwestern.edu>
2022-10-14 21:56:00 -0500

    Initial commit

---
+++ app/src/main/java/edu/missouriwestern/csmp/cwilson39/movielayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.missouriwestern.csmp.cwilson39.movielayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
